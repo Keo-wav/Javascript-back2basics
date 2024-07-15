49ebd160b4d11f6838876bced25b26e9e52523bb
Nicolas NACKPHOUMINH <n.nackphouminh@gmail.com>
2024-07-15 15:20:04 +0200

    typescript returns. functions tests and started mingling with tab declaration and functions for the Batosan project

--- ts/index.js
+++ ts/index.js
@@ -1,27 +1,69 @@
-var aString = 'Hi you fucking nice person';
-console.log(aString);
-var isFake = true;
-console.log(!isFake);
-var devil = 666;
-console.log(devil * 2);
-var cardinalPoints;
-(function (cardinalPoints) {
-    cardinalPoints[cardinalPoints["N"] = 0] = "N";
-    cardinalPoints[cardinalPoints["S"] = 1] = "S";
-    cardinalPoints[cardinalPoints["E"] = 2] = "E";
-    cardinalPoints[cardinalPoints["W"] = 3] = "W";
-})(cardinalPoints || (cardinalPoints = {}));
-var north = cardinalPoints.N;
-var south = cardinalPoints.S;
-var east = cardinalPoints.E;
-var west = cardinalPoints.W;
-console.log(north);
-console.log(south);
-console.log(east);
-console.log(west);
-var tab = [6, 12, 666];
-console.log(tab);
-var noType;
-console.log(noType);
-var anyType;
-console.log(anyType);
+// let aString : string = 'Hi you fucking nice person'
+// console.log(aString)
+// let isFake : boolean = true
+// console.log(!isFake)
+// let devil : number = 666
+// console.log(devil *2)
+// enum cardinalPoints {
+//     N,
+//     S, 
+//     E,
+//     W
+// }
+// let north : cardinalPoints = cardinalPoints.N
+// let south : cardinalPoints = cardinalPoints.S
+// let east : cardinalPoints = cardinalPoints.E
+// let west : cardinalPoints = cardinalPoints.W
+// console.log(north)
+// console.log(south)
+// console.log(east)
+// console.log(west)
+// let tab : number[] = [6, 12, 666, 12, 12]
+// console.log(tab)
+// let noType : void
+// console.log(noType)
+// let anyType : any = 6
+// console.log(anyType)
+// can assign a value to anyType because it can have .. well .. ANY type.
+// on the other hand the noType value does not have a type so we can't assign it anything (error inc)
+// named function 
+// console.log('named function :')
+// function Add(x:number, y:number):number {
+//     return x + y
+// }
+// function SaltyGreet(name:string):string {
+//     let rand:number = Math.round(Math.random()*50)
+//     return "Hey there, " + name + ", your stupid ass has to spend " + rand + " years to even being okay at coding, so get cracking you nutbust"
+// }
+// console.log(Add(5, 7))
+// console.log(SaltyGreet('Nicolas'))
+// anonymous function with lambda
+var complexOperation = function (a, b, c) {
+    var result = 0;
+    result += a * c;
+    result /= b;
+    return result;
+};
+console.log(complexOperation(6, 9, 3));
+var words = ['thank you', 'arigato', 'hello', 'konichiwa', 'my name is', 'watashiwa', 'damn', 'kuso', 'pervert', 'hentai'];
+function EngWords(tab) {
+    var englishWordsTab = [];
+    for (var i = 0; i < tab.length; i++) {
+        if (i % 2 === 0) {
+            englishWordsTab.push(tab[i]);
+        }
+    }
+    return englishWordsTab;
+}
+function JapWords(tab) {
+    var japaneseWordsTab = [];
+    for (var i = 0; i < tab.length; i++) {
+        if (i % 2 !== 0) {
+            japaneseWordsTab.push(tab[i]);
+        }
+    }
+    return japaneseWordsTab;
+}
+// console.log(words)
+// console.log(EngWords(words))
+// console.log(JapWords(words))
--- ts/index.ts
+++ ts/index.ts
@@ -1,36 +1,82 @@
-let aString : string = 'Hi you fucking nice person'
-console.log(aString)
+// let aString : string = 'Hi you fucking nice person'
+// console.log(aString)
 
-let isFake : boolean = true
-console.log(!isFake)
+// let isFake : boolean = true
+// console.log(!isFake)
 
-let devil : number = 666
-console.log(devil *2)
+// let devil : number = 666
+// console.log(devil *2)
 
-enum cardinalPoints {
-    N,
-    S, 
-    E,
-    W
-}
+// enum cardinalPoints {
+//     N,
+//     S, 
+//     E,
+//     W
+// }
 
-let north : cardinalPoints = cardinalPoints.N
-let south : cardinalPoints = cardinalPoints.S
-let east : cardinalPoints = cardinalPoints.E
-let west : cardinalPoints = cardinalPoints.W
-console.log(north)
-console.log(south)
-console.log(east)
-console.log(west)
+// let north : cardinalPoints = cardinalPoints.N
+// let south : cardinalPoints = cardinalPoints.S
+// let east : cardinalPoints = cardinalPoints.E
+// let west : cardinalPoints = cardinalPoints.W
+// console.log(north)
+// console.log(south)
+// console.log(east)
+// console.log(west)
 
-let tab : number[] = [6, 12, 666]
-console.log(tab)
+// let tab : number[] = [6, 12, 666, 12, 12]
+// console.log(tab)
 
-let noType : void
-console.log(noType)
+// let noType : void
+// console.log(noType)
 
-let anyType : any = 6
-console.log(anyType)
+// let anyType : any = 6
+// console.log(anyType)
 
 // can assign a value to anyType because it can have .. well .. ANY type.
 // on the other hand the noType value does not have a type so we can't assign it anything (error inc)
+
+// named function 
+// console.log('named function :')
+// function Add(x:number, y:number):number {
+//     return x + y
+// }
+// function SaltyGreet(name:string):string {
+//     let rand:number = Math.round(Math.random()*50)
+//     return "Hey there, " + name + ", your stupid ass has to spend " + rand + " years to even being okay at coding, so get cracking you nutbust"
+// }
+// console.log(Add(5, 7))
+// console.log(SaltyGreet('Nicolas'))
+
+// function with lambda
+// let complexOperation = (a:number, b:number, c:number) => {
+//     let result:number = 0
+//     result += (a * c) / b
+//     return result
+// }
+// console.log(complexOperation(6, 9, 3))
+
+let words: string[] = ['thank you', 'arigato', 'hello', 'konichiwa', 'my name is', 'watashiwa', 'damn', 'kuso', 'pervert', 'hentai']
+
+function EngWords(tab:string[]):string[] {
+    let englishWordsTab: string[] = []
+    for (let i = 0; i<tab.length; i++) {
+        if (i % 2 === 0) {
+            englishWordsTab.push(tab[i])
+        }
+    }
+    return englishWordsTab
+}
+
+function JapWords(tab:string[]):string[] {
+    let japaneseWordsTab: string[] = []
+    for (let i = 0; i<tab.length; i++) {
+        if (i % 2 !== 0) {
+            japaneseWordsTab.push(tab[i])
+        }
+    }
+    return japaneseWordsTab
+}
+
+// console.log(words)
+// console.log(EngWords(words))
+// console.log(JapWords(words))
